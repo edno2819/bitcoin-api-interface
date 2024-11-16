698272fe30dba60896ed80098bef212c4943cd8c
edno <edno2819@hotmail.com>
2024-11-15 23:47:15 -0300

    feat: Create load config module

---
+++ Makefile
@@ -0,0 +1,2 @@
+install:
+	go get "github.com/joho/godotenv"
--- src/utils/config.go
+++ src/utils/config.go
@@ -1,41 +1,87 @@
 package utils
 
 import (
-	"fmt"
+	"os"
+	"strconv"
 	"time"
-)
 
-const (
-	hostNodeBTC  = "1212.154.54"
-	loginNodeBTC = ""
-)
-
-var (
-	urlBTC = fmt.Sprintf("https://%s:%s@127.0.0.1:8332", loginNodeBTC, hostNodeBTC)
+	"github.com/joho/godotenv"
 )
 
 type RPCConfig struct {
-	hostNodeBTC    string
-	loginNodeBTC   string
-	urlBTC         string
-	ServerPingTime time.Duration `long:"server-ping-time" description:"How long the server waits on a gRPC stream with no activity before pinging the client."`
+	Host        string
+	Port        string
+	RPCUser     string
+	RPCPassword string
+	IsHTTPS     bool
 }
 
 type Config struct {
-	nodeType      string    
-	walletVersion string    
-	rpcConfig     RPCConfig
+	NodeType       string
+	WalletVersion  string
+	RPCConfig      RPCConfig
+	ServerPingTime time.Duration
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		nodeType:      "mainnet",
-		walletVersion: "2.0",
-		rpcConfig : &RPCConfig{
-			hostNodeBTC:    hostNodeBTC,
-            loginNodeBTC:   loginNodeBTC,
-            urlBTC:         urlBTC,
-            ServerPingTime: 10 * time.Second,
-		}
+		NodeType:       "mainnet",
+		WalletVersion:  "2.0",
+		ServerPingTime: 10 * time.Second,
+		RPCConfig: RPCConfig{
+			Host:        "127.0.0.1",
+			Port:        "8332",
+			RPCUser:     "user",
+			RPCPassword: "password",
+			IsHTTPS:     false,
+		},
 	}
+}
+
+func LoadConfig() (*Config, error) {
+	_ = godotenv.Load()
+
+	config := DefaultConfig()
+
+	if value, exists := os.LookupEnv("NODE_TYPE"); exists && value != "" {
+		config.NodeType = value
+	}
+
+	if value, exists := os.LookupEnv("WALLET_VERSION"); exists && value != "" {
+		config.WalletVersion = value
+	}
+
+	if value, exists := os.LookupEnv("RPC_HOST"); exists && value != "" {
+		config.RPCConfig.Host = value
+	}
+
+	if value, exists := os.LookupEnv("RPC_PORT"); exists && value != "" {
+		config.RPCConfig.Port = value
+	}
+
+	if value, exists := os.LookupEnv("RPC_USER"); exists && value != "" {
+		config.RPCConfig.RPCUser = value
+	}
+
+	if value, exists := os.LookupEnv("RPC_PASSWORD"); exists && value != "" {
+		config.RPCConfig.RPCPassword = value
+	}
+
+	if value, exists := os.LookupEnv("RPC_IS_HTTPS"); exists && value != "" {
+		isHTTPS, err := strconv.ParseBool(value)
+		if err != nil {
+			return nil, err
+		}
+		config.RPCConfig.IsHTTPS = isHTTPS
+	}
+
+	if value, exists := os.LookupEnv("SERVER_PING_TIME"); exists && value != "" {
+		duration, err := time.ParseDuration(value)
+		if err != nil {
+			return nil, err
+		}
+		config.ServerPingTime = duration
+	}
+
+	return config, nil
 }
